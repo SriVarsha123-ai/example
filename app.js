a6011833f411da6155b59964c3fabf031fbddec9
SriVarsha123-ai <srivarshabandari2005@gmail.com>
2025-11-09 12:50:22 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const app=express();
-const PORT=process.env.PORT || 3000;
-app.get('/',(req,res)=>{
-res.send('<h1>Hello,World!<h1>');
-});
-app.listen(PORT,()=>{
- console.log('server is running on http://localhost:${PORT}');
-});
